20e90541e5a6366a3da0beafe96d64aa989585c4
Daniel Phiri <malgamves@gmail.com>
2018-07-15 15:01:56 +0530

    Some docs

--- TwilioSMSUTF-8/Program.cs
+++ TwilioSMSUTF-8/Program.cs
@@ -10,27 +10,29 @@ namespace YourNewConsoleApp
     {
         static void Main(string[] args)
         {
-            // Find your Account Sid and Auth Token at twilio.com/console
+            // Find your Account Sid and Auth Token at twilio.com/console and add them below
             const string accountSid = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
             const string authToken = "xxxxxxxxxxxxxxxxxxxxxxxxxxx";
             TwilioClient.Init(accountSid, authToken);
 
             Console.WriteLine("\nGet ready to send a text! ");
 
-            Console.WriteLine("\nEnter your reciepient ");
+            // Read the number you want to send a text to
+            Console.WriteLine("\nEnter your reciepient.. ");
             var rec = Console.ReadLine();
 
-            Console.WriteLine("\nEnter your message? ");
+            // Reads the message you want to send 
+            Console.WriteLine("\nEnter your message.. ");
             var text = Console.ReadLine();
 
             var to = new PhoneNumber(rec);
             var message = MessageResource.Create(
                 to,
-                from: new PhoneNumber("+17192496515"),
+                from: new PhoneNumber("[PHONE NUMBER]"), // Add your registred Twilio number here
                 body: text);
 
             Console.WriteLine(message.Sid);
-            Console.Write("\nPress any key to exit...");
+            Console.Write("\nSuccess! Press any key to exit...");
             Console.ReadKey(true);
         }
     }

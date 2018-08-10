a02034168718c2a3788bda814df46b5fea9cc36e
Daniel Phiri <malgamves@gmail.com>
2018-08-10 15:46:24 +0530

    Added escape char feature

--- TwilioSMSUTF-8/Program.cs
+++ TwilioSMSUTF-8/Program.cs
@@ -3,6 +3,8 @@ using System;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
 using Twilio.Types;
+using J3QQ4; // Let's us access Emoji without using escape characters
+
 
 namespace YourNewConsoleApp
 {
@@ -11,26 +13,26 @@ namespace YourNewConsoleApp
         static void Main(string[] args)
         {
             // Find your Account Sid and Auth Token at twilio.com/console and add them below
-            const string accountSid = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-            const string authToken = "xxxxxxxxxxxxxxxxxxxxxxxxxxx";
+            const string accountSid = "<Your Twilio Account SID>";
+            const string authToken = "<Your Twilio Auth Token>";
             TwilioClient.Init(accountSid, authToken);
 
-            Console.WriteLine("\nGet ready to send a text! ");
+            // Add the number you want to send a text to here
+            var rec = "<Your reciepients phone number";
 
-            // Read the number you want to send a text to
-            Console.WriteLine("\nEnter your reciepient.. ");
-            var rec = Console.ReadLine();
-
-            // Reads the message you want to send 
-            Console.WriteLine("\nEnter your message.. ");
-            var text = Console.ReadLine();
+            // Add the message you want to send here
+            // It's important to note the escape characters being used as well as the way we add a Blush Emoji ( More info in the docs ) 
+            string text = "\"The \u00C6olean Harp\", by Samuel Taylor Coleridge is one of my favourite books! \u03C8" + Emoji.Blush + "\n \u4EF0 is \u26C4 in Japanese ( jk. \u270B)";
 
             var to = new PhoneNumber(rec);
+
+            // Putting everything together and sending the text
             var message = MessageResource.Create(
                 to,
-                from: new PhoneNumber("[PHONE NUMBER]"), // Add your registred Twilio number here
+                from: new PhoneNumber("<Your Twilio Phone Number>"), // Add your registred Twilio number here
                 body: text);
 
+            Console.WriteLine("\nYou just sent a text with Twilio *Mic Drop* ");
             Console.WriteLine(message.Sid);
             Console.Write("\nSuccess! Press any key to exit...");
             Console.ReadKey(true);
